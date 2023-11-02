7d51468c8b4fd013fc0e5e0418fdc8a622443f84
nt <>
2023-11-02 19:42:39 +0000

    static ffmpeg

--- get_ytarchive.sh
+++ get_ytarchive.sh
@@ -1,3 +1,3 @@
-wget -O ytarchive_linux_amd64.zip "https://github.com/Kethsar/ytarchive/releases/download/latest/ytarchive_linux_amd64.zip"
-unzip ytarchive_linux_amd64.zip
-rm -rvf ytarchive_linux_amd64.zip
+wget -O ytarchive_linux_amd64.zip "https://github.com/Kethsar/ytarchive/releases/download/latest/ytarchive_linux_amd64.zip" && \
+    unzip -o ytarchive_linux_amd64.zip && \
+    rm -rvf ytarchive_linux_amd64.zip
